dae1bb6d223fa3ff3d84036ca4a95272f80deccf
siva1234s <kotasivareddy30@gmail.com>
2022-04-28 14:47:47 +0530

    javaprojects

---
+++ Assignment6/src/com/sivareddy/assignment6/Employee.java
@@ -0,0 +1,24 @@
+package com.sivareddy.assignment6;
+
+public class Employee {
+	private int empid;
+	private String  ename, address;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
