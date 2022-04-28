82be6fc9fb324eb968fc082aeeab95a52eba3512
Sivaredddy <104560170+Sivaredddy@users.noreply.github.com>
2022-04-28 18:33:09 +0530

    Add files via upload

---
+++ ThreadCounter.java
@@ -0,0 +1,14 @@
+package com.sivareddy.assignment8;
+
+public class ThreadCounter {
+
+	public static void main(String args[])
+
+	{
+
+	Counter counter = new Counter(25);
+
+	counter.run();
+
+	}
+}
